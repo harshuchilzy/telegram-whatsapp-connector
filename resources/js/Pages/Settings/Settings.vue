da7e2634eadcaf8b25fc6591930a98a103cf8e61
KusalDay <106142440+KusalDay@users.noreply.github.com>
2022-09-15 14:39:18 +0530

    Settings styles

--- resources/js/Pages/Settings/Settings.vue
+++ resources/js/Pages/Settings/Settings.vue
@@ -30,7 +30,8 @@
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 bg-white border-b border-gray-200">
                             <form @submit.prevent="submit" class="px-8 pt-6 pb-8 mb-4">
-                                <div class="mb-4">
+                                <div class="grid grid-cols-2 gap-4">
+                                    <div class="mb-4">
                                     <label class="block text-gray-700 text-sm font-bold mb-2" for="match_case">
                                         Full Name
                                     </label>
@@ -44,7 +45,9 @@
                                     <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="match_case" name="match_case" type="text" 
                                     placeholder="Addess">
                                 </div>
-                                <div class="mb-4">
+                                </div>
+                                 <div class="grid grid-cols-2 gap-4">
+                                     <div class="mb-4">
                                     <label class="block text-gray-700 text-sm font-bold mb-2" for="match_case">
                                         Email
                                     </label>
@@ -58,9 +61,11 @@
                                     <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="match_case" name="match_case" type="text" 
                                     placeholder="Company">
                                 </div>
+                                </div>
+                               
                                 <div class="flex items-center justify-between">
                                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                                        Create
+                                        Update Settings
                                     </button>
                                 </div>
                             </form>
