f544d6242ee79905f2fce5e9a2ac810a9c779655
KusalDay <106142440+KusalDay@users.noreply.github.com>
2022-09-21 18:06:04 +0530

    message table changes

--- resources/js/Pages/Messages/Index.vue
+++ resources/js/Pages/Messages/Index.vue
@@ -25,9 +25,50 @@
     
             <div class="py-12">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="grid grid-cols-10 gap-4">
+                            <div class="p-6 bg-white border-b border-gray-200 col-span-5"> 
+                                <h1 class="mb-4">Accepted</h1>        
+                                <table class="table w-full text-sm text-left text-gray-500 dark:text-gray-400" id="datatable_1">
+                                    <thead class="text-xs text-white uppercase bg-gray-50 dark:bg-slate-700 dark:text-white">
+                                        <tr>
+                                            <th scope="col" class="px-6 py-3">
+                                                Derections
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Sender
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Message
+                                            </th>
+                                            <th scope="col" class="px-6 py-3">
+                                                Action
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="accepted in accepted" :key="accepted.id" class="bg-white border-b border-gray-100 text-black">
+                                            <th scope="row" class="px-6 py-4 font-medium text-black dark:text-dark whitespace-nowrap">
+                                                {{ accepted.direction }}
+                                            </th>
+                                            <td class="px-6 py-4">
+                                                {{accepted.sender}}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                {{accepted.message}}
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                {{accepted.action}}
+                                            </td>
+                                        
+                                        </tr>
+                                    </tbody>
+                                </table> 
+                            
+                            </div>
+
                             <div class="p-6 bg-white border-b border-gray-200 col-span-5">
+                                <h1 class="mb-4">Rejected</h1>      
                                 <table class="table w-full text-sm text-left text-gray-500 dark:text-gray-400" id="datatable_1">
                                     <thead class="text-xs text-white uppercase bg-gray-50 dark:bg-slate-700 dark:text-white">
                                         <tr>
@@ -66,44 +107,7 @@
                                 </table> 
                             
                             </div>
-                            <div class="p-6 bg-white border-b border-gray-200 col-span-3">         
-                                <table class="table w-full text-sm text-left text-gray-500 dark:text-gray-400" id="datatable_1">
-                                    <thead class="text-xs text-white uppercase bg-gray-50 dark:bg-slate-700 dark:text-white">
-                                        <tr>
-                                            <th scope="col" class="px-6 py-3">
-                                                Derections
-                                            </th>
-                                            <th scope="col" class="px-6 py-3">
-                                                Sender
-                                            </th>
-                                            <th scope="col" class="px-6 py-3">
-                                                Message
-                                            </th>
-                                            <th scope="col" class="px-6 py-3">
-                                                Action
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr v-for="accepted in accepted" :key="accepted.id" class="bg-white border-b border-gray-100 text-black">
-                                            <th scope="row" class="px-6 py-4 font-medium text-black dark:text-dark whitespace-nowrap">
-                                                {{ accepted.direction }}
-                                            </th>
-                                            <td class="px-6 py-4">
-                                                {{accepted.sender}}
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                {{accepted.message}}
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                {{accepted.action}}
-                                            </td>
-                                        
-                                        </tr>
-                                    </tbody>
-                                </table> 
                             
-                            </div>
                         </div>
                     </div>
                 </div>
